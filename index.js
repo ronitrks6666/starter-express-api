4e2f2b4458b09885b096465d50ae366ff9a018ee
Ronit Kumar <95290276+ronitrks6666@users.noreply.github.com>
2023-05-19 19:29:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const path = require('path')
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("App running")
 })
-app.listen(process.env.PORT || 3000)
+
+app.post('/bfhl', (req, res) => {
+    const data = req.body.data
+    var alphabetsArray = [];
+    var numbersArray = [];
+
+    if (data) {
+        data.forEach(item => {
+            if (isNaN(item)) {
+                alphabetsArray.push(item);
+            } else {
+                numbersArray.push(item);
+            }
+        })
+    }
+
+    res.send({ is_success: "true", user_id: "ronitkumar_07042001", email: "rk1097@srmist.edu.in", roll_number: "RA2011033010108", numbers: numbersArray, alphabets: alphabetsArray })
+})
+
+
+app.get('/bfhl', (req, res) => {
+    res.json({ operation_code: 1 })
+})
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+app.listen(server_port, server_host, (req, res) => {
+    console.log("Server is running :]")
+})
